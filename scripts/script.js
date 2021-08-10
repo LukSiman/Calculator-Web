8c37e0efe8ebb495417226fab0e64d430871756c
LukSiman <simanavicius.lukas@gmail.com>
2021-08-10 16:08:24 -0700

    added keyboard support for solving

--- scripts/script.js
+++ scripts/script.js
@@ -14,17 +14,22 @@ let operator;
 //Takes keyboard inputs and calls the correct function
 document.addEventListener('keydown', (key) => {
     let button = key.key;
- 
-    if (isFinite(button)) {        
+
+    if (isFinite(button)) {
         numberLogic(button);
-    } else if(button.match(/^(\*|\-|\+|\/)$/)){
+    } else if (button.match(/^(\*|\-|\+|\/)$/)) {
         operatorLogic(button);
-    } else if(button.match(/^(\.)$/)){
+    } else if (button.match(/^(\.)$/)) {
         decimalLogic(button);
-    }  else if(button.match(/^(Enter|\=)$/)){
-       console.log(button);
-    }  
-    
+    } else if (button.match(/^(Enter|\=)$/)) {
+        solveLogic();
+    } else if(button.match(/^(Backspace)$/)){
+        console.log(button);
+
+    }
+
+    //add click on keyboard?
+
 });
 
 //Add numbers to the display
@@ -34,12 +39,36 @@ numbers.forEach((button) => {
     });
 });
 
+//Functionality for number buttons
+function numberLogic(button) {
+    if (clearedValue) {
+        display.textContent = button;
+        clearedValue = false;
+    } else {
+        if (display.textContent.length <= 14) {
+            display.textContent += button;
+        }
+    }
+}
+
 //Set the operator
 operators.forEach((button) => {
     button.addEventListener('click', () => {
         operatorLogic(button.textContent);
     });
 });
+
+//Functionality for operator buttons
+function operatorLogic(button) {
+    checkOperandPosition();
+
+    // if operator was already set solve the operation
+    if (operator !== undefined && secondOperand !== undefined) {
+        solveDisplay();
+    }
+
+    operator = button;
+};
 
 //Add decimal if no decimals are already added
 decimal.addEventListener('click', () => {
@@ -55,19 +84,22 @@ function decimalLogic(button) {
 }
 
 //Solve the operation
-solve.addEventListener('click', () =>{
+solve.addEventListener('click', solveLogic);
+
+//Functionality for solving the calculation
+function solveLogic() {
     checkOperandPosition();
 
-    if(!clearedValue && operator !== undefined){
+    if (!clearedValue && operator !== undefined) {
         solveDisplay();
 
-        if(display.textContent.length >16){
+        if (display.textContent.length > 16) {
             display.style.fontSize = '21px';
         }
 
         clearValues();
     }
-});
+}
 
 //Clear the display
 clear.addEventListener('click', () => {
@@ -78,36 +110,14 @@ clear.addEventListener('click', () => {
 
 //Backspace on entered numbers
 back.addEventListener('click', () => {
-    display.textContent = display.textContent.slice(0, length-1);
-    if(display.textContent === ''){
+    display.textContent = display.textContent.slice(0, length - 1);
+    if (display.textContent === '') {
         display.textContent = 0;
         clearedValue = true;
     }
 });
 
-//Functionality for number buttons
-function numberLogic(button) {
-    if (clearedValue) {
-        display.textContent = button;
-        clearedValue = false;
-    } else {
-        if (display.textContent.length <= 14) {
-            display.textContent += button;
-        }
-    }
-}
 
-//Functionality for operator buttons
-function operatorLogic(button){
-    checkOperandPosition();
-
-    // if operator was already set solve the operation
-    if (operator !== undefined && secondOperand !== undefined) {
-        solveDisplay();
-    }
-
-    operator = button;
-};
 
 //Sets first and second operand according to operator value
 function checkOperandPosition() {
@@ -116,16 +126,16 @@ function checkOperandPosition() {
         clearedValue = true;
     } else {
         secondOperand = display.textContent;
-    }    
+    }
 }
 
 //Solves the operation
-function solveDisplay(){
+function solveDisplay() {
     display.textContent = operate(operator, Number(firstOperand), Number(secondOperand));
 }
 
 //Clears values to default 
-function clearValues(){
+function clearValues() {
     clearedValue = true;
     operator = undefined;
     firstOperand = undefined;
@@ -133,8 +143,8 @@ function clearValues(){
 }
 
 //Calls math function according to operator value
-function operate(operator, numberOne, numberTwo){
-    switch(operator){
+function operate(operator, numberOne, numberTwo) {
+    switch (operator) {
         case "+":
             return add(numberOne, numberTwo);
         case "-":
@@ -147,23 +157,23 @@ function operate(operator, numberOne, numberTwo){
 }
 
 //Calculates addition
-function add(numberOne, numberTwo){
+function add(numberOne, numberTwo) {
     return Math.round((numberOne + numberTwo) * 100) / 100;
 }
 
 //Calculates subtraction
-function subtract(numberOne, numberTwo){
+function subtract(numberOne, numberTwo) {
     return Math.round((numberOne - numberTwo) * 100) / 100;
 }
 
 //Calculates multiplication
-function multipy(numberOne, numberTwo){
+function multipy(numberOne, numberTwo) {
     return Math.round((numberOne * numberTwo) * 100) / 100;
 }
 
 //Calculates division
-function divide(numberOne, numberTwo){
-    if(secondOperand == 0){
+function divide(numberOne, numberTwo) {
+    if (secondOperand == 0) {
         display.style.fontSize = '19px';
         return 'Nuclear missile launched!';
     }
